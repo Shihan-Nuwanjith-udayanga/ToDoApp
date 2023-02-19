896fcc475d53f8b9c6bbc1bfdba2e1e72bd4ee39
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-02-19 22:12:29 +0530

    add a new colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+const Color bgBlue = Color(0x85c5f3);
+const Color bgGreen = Color(0xC5E00);
+const Color bgRed = Color(0xA10000);
+
+const Color txtWhite = Color(0xFFFFFF);
+const Color txtBlack = Color(0x0);
+const Color txtBlueD = Color(0x2035D);
+const Color txtBlueL = Color(0x4647F1);
